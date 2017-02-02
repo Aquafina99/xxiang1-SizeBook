948601b60c143498138851a8d5c6c75485349ebb
Aquafina99 <xxiang1@ualberta.ca>
2017-02-01 22:14:30 -0700

    added second UI page; added RecordActivity; updated MainActivity

--- app/src/main/java/com/example/android/xxiang1_sizebook/MainActivity.java
+++ app/src/main/java/com/example/android/xxiang1_sizebook/MainActivity.java
@@ -1,15 +1,65 @@
 package com.example.android.xxiang1_sizebook;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.icu.text.AlphabeticIndex;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String FILENAME = "file.sav";
+
+    private ListView RecordList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //set up Add Button to open RecordActivity
+        Button addNew = (Button) findViewById(R.id.add);
+        addNew.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecordActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/android/xxiang1_sizebook/RecordActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.xxiang1_sizebook;
+
+import android.icu.text.AlphabeticIndex;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Created by ceciliaxiang on 2017-02-01.
+ */
+
+public class RecordActivity extends AppCompatActivity {
+
+
+    private EditText nameEditText;
+    private EditText dateEditText;
+    private EditText neckEditText;
+    private EditText bustEditText;
+    private EditText chestEditText;
+    private EditText waistEditText;
+    private EditText hipEditText;
+    private EditText inseamEditText;
+    private EditText commentEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+
+        // Find the views that we will need to read user input from
+        nameEditText= (EditText) findViewById(R.id.name_view);
+        dateEditText = (EditText) findViewById(R.id.date_view);
+        neckEditText = (EditText) findViewById(R.id.edit_neck);
+        bustEditText = (EditText) findViewById(R.id.edit_bust);
+        chestEditText= (EditText) findViewById(R.id.edit_chest);
+        waistEditText = (EditText) findViewById(R.id.edit_waist);
+        hipEditText = (EditText) findViewById(R.id.edit_hip);
+        inseamEditText = (EditText) findViewById(R.id.edit_inseam);
+        commentEditText = (EditText) findViewById(R.id.comment_view);
+
+    }
+
+}
